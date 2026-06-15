d1ce3e8a16624b87978e886180fbd816e25e9dbe
Kelyan PEGEOT SELME <75201282+kelyaenn@users.noreply.github.com>
2026-06-15 08:52:35 +0200

    hotfix: clean design (#48)

--- src/packages/kelec-model/view/Button.tsx
+++ src/packages/kelec-model/view/Button.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { NEUTRAL_200, NEUTRAL_ZERO } from "../lib/colours";
 import Text from "../../../screen/Common/CustomText";
-import { ActivityIndicator, TouchableOpacity } from "react-native";
+import { ActivityIndicator, TouchableOpacity, View } from "react-native";
 import Icon from "react-native-vector-icons/MaterialIcons";
-import { subTitle2 } from "./Titles";
-import { spacerL, spacerM, spacerS, spacerXL } from "./Spacers";
+import { subTitle } from "./Titles";
+import { spacerL, spacerM, spacerXL } from "./Spacers";
+import { ButtonColours, ButtonColoursPalettes } from "../lib/buttonTypes";
+import LinearGradient from "react-native-linear-gradient";
 
 type ButtonProps = {
-    colour: string;
+    buttonColour: ButtonColours;
     text: string;
     onPress: () => void;
     disabled?: boolean;
@@ -17,16 +18,19 @@ type ButtonProps = {
 }
 
 const Button = (props: ButtonProps): React.JSX.Element => {
-    const { colour, text, onPress, disabled, isLoading, testID, iconName } = props;
+    const { buttonColour, text, onPress, disabled, isLoading, testID, iconName } = props;
+
+    const colourToApply = disabled ? ButtonColours.DISABLED : buttonColour;
+    const getCurrentColour = ButtonColoursPalettes[colourToApply];
 
     const getButtonContent = () => {
         if (isLoading) {
             return <ActivityIndicator />;
         }
         if (iconName) {
-            return <Icon name={iconName} size={spacerL} color={colour === NEUTRAL_ZERO ? 'black' : 'white'} />;
+            return <Icon name={iconName} size={spacerL} color={getCurrentColour.textColour} />;
         } else {
-            return <Text style={subTitle2}>{text}</Text>;
+            return <Text style={[subTitle, { color: getCurrentColour.textColour, fontWeight: '400' }]}>{text}</Text>;
         }
     };
 
@@ -35,21 +39,32 @@ const Button = (props: ButtonProps): React.JSX.Element => {
         <TouchableOpacity
             onPress={onPress}
             style={{
-                backgroundColor: disabled ? NEUTRAL_200 : colour,
-                borderRadius: spacerS,
-                alignItems: 'center',
-                justifyContent: 'center',
-                paddingVertical: spacerM,
-                paddingHorizontal: spacerXL,
-                borderWidth: colour === NEUTRAL_ZERO ? 1 : 0,
+                borderRadius: spacerM,
+                borderWidth: getCurrentColour.borderWidth,
                 borderColor: '#CCCCCC',
                 alignSelf: 'stretch',
-                flexGrow: 1
+                overflow: 'hidden',
+
             }}
             disabled={disabled || isLoading}
             testID={testID}
         >
-            {getButtonContent()}
+            <LinearGradient
+                colors={getCurrentColour.backgroundColour}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 0 }}
+            >
+                <View style={{
+                    paddingVertical: spacerM,
+                    paddingHorizontal: spacerXL,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    flexDirection: 'row',
+
+                }}>
+                    {getButtonContent()}
+                </View>
+            </LinearGradient>
 
         </TouchableOpacity>
     )
